eb78cb13c6cb141a83acdb4b6e8bce035a341357
rhizodev <eric@rhizo.dev>
2023-11-06 17:44:45 -0700

    add test function

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use rhizosdk::fn_main;
 use rhizosdk::container::*;
 
 fn_main!(|buffer: Vec<u8>|{
-    b"Helo, world!".to_vec()
+    b"Hello, world!".to_vec()
 });
+
+#[no_mangle]
+pub fn test() {
+    assert_eq(true, true);
+}
